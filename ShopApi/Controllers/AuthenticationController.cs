4bf1817ada07c14d97de27d723f027a90a5c1fe3
Evan Buss <evan.buss28@gmail.com>
2019-07-09 12:29:37 -0400

    Extract auth business logic to a service class

--- ShopApi/Controllers/AuthenticationController.cs
+++ ShopApi/Controllers/AuthenticationController.cs
@@ -1,28 +1,22 @@
 using System;
-using System.Text;
 using Microsoft.AspNetCore.Mvc;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.Cryptography.KeyDerivation;
-using System.Collections.Generic;
-using System.Linq;
-
 using ShopApi.Models;
+using ShopApi.Services;
 
 namespace ShopApi.Controllers
 {
 
+    // Controls user sign-ups and log-ins
     public class AuthenticationController : ControllerBase
     {
-
         private readonly FamilyShopContext _context;
+        private readonly AuthenticationService _authService;
 
         public AuthenticationController(FamilyShopContext context)
         {
             _context = context;
+            _authService = new AuthenticationService(_context);
         }
 
         // Login to an existing account
@@ -35,11 +29,11 @@ namespace ShopApi.Controllers
             {
                 return BadRequest("Invalid email or password");
             }
-            var savedPasswordhash = hashPassword(user.passwordSalt, password);
+            var savedPasswordhash = _authService.hashPassword(user.passwordSalt, password);
             Console.WriteLine("GEN: " + savedPasswordhash + "  SAVED: " + user.passwordHash);
             if (savedPasswordhash == user.passwordHash)
             {
-                return new ObjectResult(GenerateToken(email));
+                return new ObjectResult(_authService.GenerateToken(email));
             }
             return BadRequest("Invalid username or password.");
         }
@@ -55,21 +49,20 @@ namespace ShopApi.Controllers
         [HttpPost]
         public async Task<IActionResult> SignUp(string username, string password, string email)
         {
-
             // 1. Ensure email isn't used
-            if (!emailAvailable(email))
+            if (!_authService.emailAvailable(email))
             {
                 return BadRequest("Account with username already exists.");
             }
             // 2. Ensure password meets guidelines
-            if (!validPassword(password))
+            if (!_authService.validPassword(password))
             {
                 return BadRequest("Passwords must be atleast 6 characters.");
             }
             // 3. Generate password salt.
-            var salt = generateSalt();
+            var salt = _authService.generateSalt();
             // 4. Hash salt+password
-            var hashedPassword = hashPassword(salt, password);
+            var hashedPassword = _authService.hashPassword(salt, password);
 
             // 5. Store both in the database
             var newUser = new User()
@@ -82,66 +75,7 @@ namespace ShopApi.Controllers
             _context.Add(newUser);
             await _context.SaveChangesAsync();
             // 6. Return new valid JWT
-            return new ObjectResult(GenerateToken(email));
-        }
-
-        // Make sure the given password meets password criteria
-        private bool validPassword(string password)
-        {
-            if (password.Length <= 6)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        // Check to make sure no other account is using the email address
-        private bool emailAvailable(string email)
-        {
-            if (_context.Users.Where(q => q.email == email).FirstOrDefault() == null)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        // Generate a unique salt
-        private byte[] generateSalt()
-        {
-            byte[] salt = new byte[128 / 8];
-            new RNGCryptoServiceProvider().GetBytes(salt);
-            return salt;
-        }
-
-        // Generate a securely hashed password
-        private string hashPassword(byte[] salt, string password)
-        {
-            // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
-            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password: password,
-                salt: salt,
-                prf: KeyDerivationPrf.HMACSHA1,
-                iterationCount: 10000,
-                numBytesRequested: 256 / 8));
-            return hashed;
-        }
-
-        // Generate a JWT token...
-        private string GenerateToken(string username)
-        {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes("the secret that needs to be at least 16 characters long for HmacSha256bytes");
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim(ClaimTypes.NameIdentifier, username)
-                }),
-                Expires = DateTime.UtcNow.AddMinutes(1),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            return tokenHandler.WriteToken(token);
+            return new ObjectResult(_authService.GenerateToken(email));
         }
     }
 }
---
+++ ShopApi/Services/AuthenticationService.cs
@@ -0,0 +1,83 @@
+using ShopApi.Models;
+using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+namespace ShopApi.Services
+{
+    public class AuthenticationService
+    {   
+        private readonly FamilyShopContext _context;
+
+        public AuthenticationService(FamilyShopContext context) 
+        {
+            _context = context;
+        }
+        
+
+        // Make sure the given password meets password criteria
+        public bool validPassword(string password)
+        {
+            if (password.Length <= 6)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // Check to make sure no other account is using the email address
+        public bool emailAvailable(string email)
+        {
+            if (_context.Users.Where(q => q.email == email).FirstOrDefault() == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // Generate a unique salt
+        public byte[] generateSalt()
+        {
+            byte[] salt = new byte[128 / 8];
+            new RNGCryptoServiceProvider().GetBytes(salt);
+            return salt;
+        }
+
+        // Generate a securely hashed password
+        public string hashPassword(byte[] salt, string password)
+        {
+            // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+            return hashed;
+        }
+
+        // Generate a JWT token...
+        public string GenerateToken(string username)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("the secret that needs to be at least 16 characters long for HmacSha256bytes");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, username)
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
